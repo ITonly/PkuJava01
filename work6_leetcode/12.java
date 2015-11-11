45d1abbcc8fbc79bb9127367250d83cc2b0568d9
lishidi <1264433268@qq.com>
2015-11-12 06:47:41 +0800

    6th leetcode

---
+++ work6_leetcode/12.java
@@ -0,0 +1,28 @@
+/**
+ * ÂÞÂíÊý×Ö
+ * 1-9£»
+ * 10-90£»
+ * 100-900£»
+ * 1000-3000
+ * 
+ */
+public class Solution {
+    public String intToRoman(int num) {
+        String roman[][]={
+            {"","I","II","III","IV","V","VI","VII","VIII","IX"},
+            {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"},
+            {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"},
+            {"","M","MM","MMM"}
+        };
+        
+        String ret="";
+        int digit=0;
+        while(num!=0){
+            int remain=num%10;
+            ret=roman[digit][remain]+ret;
+            digit++;
+            num/=10;
+        }
+        return ret;
+    }
+}
